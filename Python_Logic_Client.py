e1072d40c7aea8972da2d36c9c5044ed69c14ca8
Serai-XIV <elitepetetv@gmail.com>
2022-10-11 18:29:40 -0700

    Python_Client

---
+++ Python_Logic_Client.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am a robot")
+
+bot.run("MTAyNDEyNjk1NTU0Njc0NjkyMg.GT8Hlp.D92ReYMR8LI8pXaNOuLRDrVC6GrZ3TZLrwto5c")
