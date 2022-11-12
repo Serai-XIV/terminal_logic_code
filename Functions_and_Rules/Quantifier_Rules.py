48b08cbec757cab9e73d6c23dca1a8e78cb38bde
Serai-XIV <elitepetetv@gmail.com>
2022-11-12 02:15:03 -0800

    Main Code

---
+++ Functions_and_Rules/Quantifier_Rules.py
@@ -0,0 +1,11 @@
+def universal_intantiation(formula="∀x(Sx ⇒ Px)", constant="a"):
+    variable = "x" or "y" or "z"
+    New_formula = (
+        formula.replace(variable, constant)
+        .replace("∀", "")
+        .replace("(", "")
+        .replace(")", "")
+    )
+    New_formula2 = New_formula.replace(constant, "", 1)
+
+    return New_formula2
