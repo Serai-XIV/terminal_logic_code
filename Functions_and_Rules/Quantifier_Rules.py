e039d300e4a802d79c507abe5c25038ef5295741
Serai-XIV <elitepetetv@gmail.com>
2022-11-14 20:45:45 -0800

    Rules for Predicate Logic

--- Functions_and_Rules/Quantifier_Rules.py
+++ Functions_and_Rules/Quantifier_Rules.py
@@ -9,3 +9,16 @@ def universal_intantiation(formula="∀x(Sx ⇒ Px)", constant="a"):
     New_formula2 = New_formula.replace(constant, "", 1)
 
     return New_formula2
+
+
+def existential_intantiation(formula="∃x(Sx ∧ Px)", constant="a"):
+    variable = "x" or "y" or "z"
+    New_formula = (
+        formula.replace(variable, constant)
+        .replace("∃", "")
+        .replace("(", "")
+        .replace(")", "")
+    )
+    New_formula2 = New_formula.replace(constant, "", 1)
+
+    return New_formula2
