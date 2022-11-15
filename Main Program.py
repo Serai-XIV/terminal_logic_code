806699305e519faa8738b34df80c13700b0043d2
Serai-XIV <elitepetetv@gmail.com>
2022-11-14 20:45:16 -0800

    Main Program

--- Main Program.py
+++ Main Program.py
@@ -1,15 +1,4 @@
-def universal_intantiation(formula="∀x(Sx ⇒ Px)", constant="a"):
-    variable = "x" or "y" or "z"
-    New_formula = (
-        formula.replace(variable, constant)
-        .replace("∀", "")
-        .replace("(", "")
-        .replace(")", "")
-    )
-    New_formula2 = New_formula.replace(constant, "", 1)
-
-    return New_formula2
-
+import Functions_and_Rules 
 
 def answer():
     user_aws = str(input("Would you like to to Enter a constant?: "))
