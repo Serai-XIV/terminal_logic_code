e0f275ae8b0cf9084cefaa8de81cab26d9974d4f
Serai-XIV <elitepetetv@gmail.com>
2023-01-07 17:23:33 -0800

    Deleted some elements within the logical sym dict.

--- functions_and_rules/Logical_Symbols_and_Semantics.py
+++ functions_and_rules/Logical_Symbols_and_Semantics.py
@@ -14,13 +14,13 @@ can be easily updated and extended to add more operators or symbols.
 # A dictionary that maps logical operators to their corresponding symbols in various notation systems
 Logical_Symbols = dict(
     # Negation: represents the logical operation of negation, which is the reversal of the truth value of a proposition
-    Negation=["¬", "~", "!", "not"],
+    Negation=["¬", "~", "!"],
     # Conjunction: represents the logical operation of conjunction, which is the combination of two propositions in such a way that the resulting proposition is only true if both of the original propositions are true
-    Conjunction=["∧", "&", "·", "and"],
+    Conjunction=["∧", "&", "·"],
     # Inclusive disjunction: represents the logical operation of inclusive disjunction, which is the combination of two propositions in such a way that the resulting proposition is true if either of the original propositions are true (or if both are true)
-    Inclusive_Disjunction=["∨", "or"],
+    Inclusive_Disjunction=["∨"],
     # Material conditional: represents the logical operation of material conditional, which is a type of conditional statement in logic that is only considered true if the antecedent is false or the consequent is true
-    Material_Conditional=["→", "⇒", "⊃", "if, then"],
+    Material_Conditional=["→", "⇒", "⊃"],
     # Material biconditional: represents the logical operation of material biconditional, which is a type of conditional statement in logic that is only considered true if the antecedent and consequent are both true or both false
     Material_Bi_Conditional=["↔", "⇔", "≡"],
     # Universal quantifier: represents the universal quantifier, which is a logical symbol used to indicate that a statement holds for all possible values of a variable
